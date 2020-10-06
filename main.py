ebaac31a5dc61ebe93480c2cc502236f8ac3bb4e
Austin Williams <amwilliams24@alaska.edu>
2020-10-05 17:45:24 -0800

    Now keeps track of configurations evaluated and prints the value for each run

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def hill_climbing_demo():
 def random_restart_demo():
     print("Random Restart Initial Board")
     problem = Problem.generate(width=30, height=20, houses=8, hospitals=2)
-    # problem = Problem.example()
     print(problem)
-    print(hill_climbing(problem))
-    print(hill_climbing(problem, True))
-    print(hill_climbing(problem, True))
-    print(hill_climbing(problem, True))
+
+    solutions = [hill_climbing(problem, True) for _ in range(4)]
+    for solution, configurations in solutions:
+        print(f"Configurations Evaluated: {configurations}")
+        print(solution)
     print('\n')
 
 
@@ -32,7 +32,11 @@ def simulated_annealing_demo():
     print("Simulated Annealing Initial Board")
     problem = Problem.generate(width=30, height=20, houses=8, hospitals=2)
     print(problem)
-    print(simulated_annealing(problem, schedule))
+
+    solutions = [simulated_annealing(problem, schedule, True) for _ in range(4)]
+    for solution, configurations in solutions:
+        print(f"Configurations Evaluated: {configurations}")
+        print(solution)
     print('\n')
 
 
--- search.py
+++ search.py
@@ -5,17 +5,23 @@ from math import exp
 
 def hill_climbing(problem, random_restart=False):
     current_state = problem.initial_state
+    configs_evaluated = 0
     if random_restart:
         current_state.random_start()
     while True:
         next_state = min(current_state.neighbors)
         if next_state >= current_state:
-            return current_state
+            return current_state, configs_evaluated
         current_state = next_state
+        configs_evaluated += 1
 
 
-def simulated_annealing(problem, schedule):
+def simulated_annealing(problem, schedule, random_restart=False):
     current_state = problem.initial_state
+    configs_evaluated = 0
+    if random_restart:
+        current_state.random_start()
+
     for t in range(1000):
         T = schedule(t)
         if T <= 0:
@@ -26,10 +32,12 @@ def simulated_annealing(problem, schedule):
         ΔE = neighbor_state - current_state
         if ΔE > 0:
             current_state = neighbor_state
+            configs_evaluated += 1
         else:
             # look at all the neighbors and pick one based on T and ΔE
             prob = exp(ΔE/T)
             rand = random()
             if prob >= rand:
                 current_state = neighbor_state
-    return current_state
+                configs_evaluated += 1
+    return current_state, configs_evaluated
